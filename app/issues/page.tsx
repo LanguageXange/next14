44748bdb7c69d7de5f3f2e835e0ea2e8db4471c1
Cindy Lin <hlin0017@seattlecentral.edu>
2024-02-19 14:42:28 -0800

    install radix theme and create new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Flex, Text, Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 const IssuePage = () => {
-  return <div>IssuePage</div>;
+  return (
+    <main>
+      <Button>
+        <Link href="/issues/new"> New Issue</Link>
+      </Button>
+    </main>
+  );
 };
 
 export default IssuePage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
 import NavBar from "./NavBar";
-
+import { Theme } from "@radix-ui/themes";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
+        <Theme>
+          <NavBar />
 
-        <main>{children}</main>
+          <main className="p-5">{children}</main>
+        </Theme>
       </body>
     </html>
   );
