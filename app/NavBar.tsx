8b664c96bb1267386ce24d0fa7b9e855ebded4b1
Cindy Lin <hlin0017@seattlecentral.edu>
2024-02-19 13:34:56 -0800

    set up navbar, prisma Issue Model

---
+++ app/NavBar.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+import { classNames } from "./utils/misc";
+import { AiFillBug } from "react-icons/ai";
+
+const NavBar = () => {
+  const pathName = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b-2 mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+
+      <ul className="flex space-x-6 ">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              className={classNames(
+                `${link.href === pathName ? "text-zinc-900" : "text-zinc-400"}`,
+                "transition-colors hover:text-blue-700"
+              )}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ app/utils/misc.ts
@@ -0,0 +1,8 @@
+export const classNames = (...names: Array<string | undefined>) => {
+  const className = names.reduce(
+    (acc, name) => (name ? `${acc} ${name}` : acc),
+    ""
+  );
+
+  return className || "";
+};
